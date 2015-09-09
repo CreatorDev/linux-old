ffd4de41ad6bb65ec763d9342f48e16bcb52a232
Bartosz Flis <bartosz.flis@imgtec.com>
2015-09-09 09:58:39 +0100

    pistachio: bt: some fixes and additions
    This includes following changes :-
    
    1. pistachio: bt: fix backlog processing
    Having backlog processing as a separate job drains work depot very
    quickly. It was always executed in fixed spots anyway, so there is no
    need for it to be a job.
    
    2. pistachio: bt: fix for destroying workqueue
    Workqueue has to be destroyed first, because doing so executes all the
    pending jobs. Those in turn need the module to be fully functional.
    
    3. pistachio: bt: add one way circular buffer implementation
    Kernel's circ_buf implementation does not allow to use the full buffer
    capacity. Add another implementation, which is compatible with the one
    used on the RPU.
    
    4. pistachio: bt: add event tracing
    Use Linux trace events architecture to profile img-bt driver.
    
    Change-Id: Iaea0006e7e34491b8012ffc9a1c012cb2c0cae68

---
+++ drivers/bluetooth/pistachio/etrace.c
@@ -0,0 +1,31 @@
+/*HEADER**********************************************************************
+ ******************************************************************************
+ ***
+ *** Copyright (c) 2011, 2012, 2013, 2014 Imagination Technologies Ltd.
+ *** All rights reserved
+ ***
+ *** This program is free software; you can redistribute it and/or
+ *** modify it under the terms of the GNU General Public License
+ *** as published by the Free Software Foundation; either version 2
+ *** of the License, or (at your option) any later version.
+ ***
+ *** This program is distributed in the hope that it will be useful,
+ *** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *** GNU General Public License for more details.
+ ***
+ *** You should have received a copy of the GNU General Public License
+ *** along with this program; if not, write to the Free Software
+ *** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ *** USA.
+ ***
+ *** File Name  : etrace.c
+ ***
+ *** File Description:
+ *** Tracepoint exports for Imagination Bluetooth driver
+ ***
+ ******************************************************************************
+ *END**************************************************************************/
+
+#define CREATE_TRACE_POINTS
+#include "etrace.h"
